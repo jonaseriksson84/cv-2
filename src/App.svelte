f98285a015389fa4f98e015ddb5459805261fdce
Jonas Eriksson <jonaseriksson84@gmail.com>
2022-05-20 13:58:31 +0200

    second commit

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import Header from './lib/Header.svelte'
+</script>
+
+<main>
+  <header>
+    <Header />
+  </header>
+  <aside>SIDEBAR</aside>
+  <section>MAIN</section>
+  <footer>FOOTER</footer>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: url('assets/table-bg.jpg');
+    background-size: cover;
+    background-position: center;
+  }
+
+  main {
+    display: grid;
+    max-width: 1200px;
+    margin: 2rem auto;
+    grid-template-areas:
+      'header  header'
+      'sidebar main  '
+      'footer  footer';
+    grid-template-columns: minmax(250px, 20%) 1fr;
+    background-image: url('assets/ivory-paper.jpg');
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+      rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+      rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  }
+  @media (max-width: 768px) {
+    main {
+      grid-template-areas:
+        'header   header'
+        'sidebar  sidebar'
+        'main     main'
+        'footer  footer';
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  header {
+    grid-area: header;
+    background-color: rgba(0, 55, 128, 0.25);
+  }
+
+  aside {
+    grid-area: sidebar;
+  }
+
+  section {
+    grid-area: main;
+    background-color: green;
+  }
+
+  footer {
+    grid-area: footer;
+    background-color: red;
+  }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import src from '../assets/jonas-bw-size.png'
+  import hiatus from '../assets/hiatus.otf'
+</script>
+
+<div class="wrapper">
+  <img {src} alt="Portrait of Jonas" />
+  <div class="title">
+    <span>Hello! My name is...</span>
+    <h1>Jonas Eriksson</h1>
+    <span>...and I am Fullstack Engineer</span>
+  </div>
+</div>
+
+<style>
+  @font-face {
+    font-family: 'hiatus';
+    src: url('../assets/hiatus.otf') format('opentype');
+  }
+  .wrapper {
+    display: grid;
+    grid-template-columns: 0.2fr 0.75fr;
+    justify-items: center;
+  }
+  img {
+    border-radius: 50%;
+    margin: 1rem;
+  }
+  .title {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: rgba(17, 12, 12, 0.75);
+  }
+  h1 {
+    font-family: 'hiatus', sans-serif;
+    font-size: 5rem;
+    margin: 0;
+  }
+  span {
+    align-self: start;
+  }
+  span:last-of-type {
+    align-self: end;
+  }
+  @media (max-width: 500px) {
+    .wrapper {
+      grid-template-columns: 1fr;
+      padding-bottom: 2rem;
+    }
+    img {
+      width: 40%;
+    }
+    h1 {
+      font-size: 3.5rem;
+      margin: 0 auto;
+    }
+    span {
+      font-size: 0.8rem;
+    }
+  }
+</style>
