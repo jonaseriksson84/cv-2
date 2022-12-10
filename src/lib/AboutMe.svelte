752e9b84e0aaebdb8ca956bda54f1eeee5eaf308
jonaseriksson84 <jonaseriksson84@gmail.com>
2022-12-10 22:55:52 +0100

    more sidebar

---
+++ src/lib/AboutMe.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  export const AboutMe = () => ({
+    Component: AboutMe,
+  })
+</script>
+
+<div>
+  <p>
+    I am a fullstack web developer with 10+ years of professional experience. I
+    particularly enjoy doing frontend development and JavaScript, though also
+    comfortable with backend and API development in a number of languages such
+    as Python, Java and server-side JavaScript.
+  </p>
+  <p>
+    I am a social person and thrive in a strong team, but I am also full of
+    initiative and drive – I will quickly learn new processes, environments and
+    technologies on my own. Regardless of project, I always carry a professional
+    attitude and clear focus on delivering results.
+  </p>
+</div>
+
+<style>
+  div {
+    margin: 0.5rem 1rem;
+    border-top: 1px solid #bababa;
+    /* a grey border top */
+  }
+  p {
+    font-size: 0.8rem;
+  }
+</style>
